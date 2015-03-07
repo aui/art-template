757dafc165a433978c327b6798b76de6ec05102a
颜海镜 <yanhaijing@yeah.net>
2015-03-07 10:50:11 +0800

    添加 noConflict方法

--- src/outro.js
+++ src/outro.js
@@ -9,6 +9,17 @@ if (typeof define === 'function') {
 } else if (typeof exports !== 'undefined') {
     module.exports = template;
 } else {
+    // Browser globals
+    var _template = this.template;
+
+    template.noConflict = function() {
+        if (this.template === template) {
+            this.template = _template;
+        }
+
+        return template;
+    };
+    
     this.template = template;
 }
 
