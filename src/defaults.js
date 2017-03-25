1c44c1e14d1104c656c0b6368dd3d3c2727c305d
糖饼 <sugarpie.tang@gmail.com>
2017-03-26 00:09:21 +0800

    修改配置

--- src/defaults.js
+++ src/defaults.js
@@ -2,10 +2,10 @@ module.exports = {
     filename: 'anonymous',
     openTag: '<%', // 逻辑语法开始标签
     closeTag: '%>', // 逻辑语法结束标签
-    output: '=',
-    unescapeOutput: '=>',
-    escape: true, // 是否编码输出变量的 HTML 字符
-    cache: true, // 是否开启缓存（依赖 options 的 filename 字段）
+    escapeOutputDirective: '=',
+    outputDirective: '=>',
+    escape: true, // 是否编码输出变量的 HTML 字符。如果为 false，escapeOutputDirective 永远不生效
+    cache: {}, // 是否开启缓存（依赖 options 的 filename 字段）
     compress: false, // 是否压缩输出
     parser: null, // 自定义语法格式器
     onerror: e => {
