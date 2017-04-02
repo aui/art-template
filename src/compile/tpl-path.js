9a3a9c6325e27e84b515458ae803ad42d9d42b7d
糖饼 <sugarpie.tang@gmail.com>
2017-04-03 02:53:09 +0800

    获取模板的绝对路径

---
+++ src/compile/tpl-path.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const isNodeEnv = typeof document !== 'object';
+
+/**
+ * 获取模板的绝对路径
+ * @param {string} filename 
+ * @param {string} root 
+ * @param {string} base 
+ * @returns {string}
+ */
+const tplPath = (filename, root, base) => {
+    if (isNodeEnv) {
+        const dirname = base ? path.dirname(base) : '';
+        return path.resolve(root, dirname, filename);
+    } else {
+        return filename;
+    }
+};
+
+module.exports = tplPath;
