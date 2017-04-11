087f1d6937d276ee41a307a7d21032c0bad53773
糖饼 <sugarpie.tang@gmail.com>
2017-04-12 00:13:40 +0800

    auto bind '.art'

--- src/bind-extname.js
+++ src/bind-extname.js
@@ -1,6 +1,7 @@
 const defaults = require('./compile/defaults');
 const detectNode = require('detect-node');
 const templatePath = require.resolve('./index');
+const EXTNAME = '.art';
 
 /**
  * 绑定模板文件后缀名，以让 NodeJS 支持 `require(templateFile)`
@@ -9,18 +10,18 @@ const templatePath = require.resolve('./index');
  */
 const bindExtname = (require, extname = defaults.extname) => {
 
-    if (detectNode) {
-        require.extensions[extname] = (module, flnm) => {
-            const filename = flnm || module.filename;
-            const imports = `var template=require(${JSON.stringify(templatePath)})`;
-            module._compile(`${imports};\module.exports = template.compile({filename:${JSON.stringify(filename)}});`, filename);
-        };
-    }
+    require.extensions[extname] = (module, flnm) => {
+        const filename = flnm || module.filename;
+        const imports = `var template=require(${JSON.stringify(templatePath)})`;
+        module._compile(`${imports};\module.exports = template.compile({filename:${JSON.stringify(filename)}});`, filename);
+    };
 
 };
 
 
-//bindExtname(require);
+if (detectNode) {
+    bindExtname(require, EXTNAME);
+}
 
 
 module.exports = bindExtname;
