2d16539fd8f166461b10333465c053fcebfb2dbd
糖饼 <sugarpie.tang@gmail.com>
2017-04-14 19:58:24 +0800

    修复art语法BUG

--- src/compile/adapter/rule.art.js
+++ src/compile/adapter/rule.art.js
@@ -1,14 +1,13 @@
 const nativeRule = {
     test: /{{([@#]?)(\/?)([\w\W]*?)}}/,
     use: function (match, raw, close, code) {
-        
+
         const compiler = this;
         const options = compiler.options;
         const esTokens = compiler.getEsTokens(code.trim());
+        const values = esTokens.map(token => token.value);
         const result = {};
-        const values = esTokens
-            .map(token => token.value)
-            .filter(value => !/^\s+$/.test(value));
+
 
         let group;
         let output = raw ? 'raw' : false;
@@ -35,15 +34,11 @@ const nativeRule = {
 
             case 'set':
 
-                // value = 0
                 code = `var ${values.join('')};`;
                 break;
 
             case 'if':
 
-                // value
-                // value + 1
-                // value.sub + 1
                 code = `if(${values.join('')}){`;
 
                 break;
@@ -51,11 +46,9 @@ const nativeRule = {
             case 'else':
 
                 if (values[0] === 'if') {
-                    // if value
                     values.shift();
                     code = `}else if(${values.join('')}){`;
                 } else {
-                    // else
                     code = `}else{`;
                 }
 
@@ -63,25 +56,16 @@ const nativeRule = {
 
             case '/if':
 
-                // /if
                 code = '}';
                 break;
 
             case 'each':
 
-                // 
-                // object
-                // object value
-                // object value index
-                // object value.list index
-
-                // ... v3 compat ...
-                // object 'as' value
-                // object 'as' value index
                 group = split(esTokens);
                 group.shift();
 
                 if (group[1] === 'as') {
+                    // ... v3 compat ...
                     upgrade('each object as value index', 'each object value index');
                     group.splice(1, 1);
                 }
@@ -96,26 +80,17 @@ const nativeRule = {
 
             case '/each':
 
-                // /each
                 code = '});';
                 break;
 
             case 'echo':
 
-                // echo value
-                // echo value value2 value3
-                // echo(value + 1, value2)
                 key = 'print';
                 upgrade('echo value', 'value');
 
             case 'print':
             case 'include':
 
-                // print value
-                // print value value2 value3
-                // include './header'
-                // include './header' context
-                // include(name + '.html', context)
                 group = split(esTokens);
                 group.shift();
                 code = `${key}(${group.join(',')});`;
@@ -126,31 +101,18 @@ const nativeRule = {
                 if (values.indexOf('|') !== -1) {
 
                     // 解析过滤器
-                    // value | filter
-                    // value|filter
-                    // value | filter 'string'
-                    // value | filter arg1 arg2 arg3
-                    // value | filter1 arg1 | filter2 | filter3
-                    // value * 1000 | filter
-                    // value[key] | filter
-                    // value1 || value2 | filter
-
-                    // ... v3 compat ...
-                    // value | filter1:'abcd' | filter2
 
                     let target = key;
                     const group = [];
-                    const v3split = ':';
+                    const v3split = ':'; // ... v3 compat ...
+                    const list = values.filter(value => !/^\s+$/.test(value));
 
                     // 找到要过滤的目标表达式
-                    // values => ['value', '[', 'key', ']', '|', 'filter1', '|', 'filter2', 'argv1', 'argv2']
-                    // target => `value[key]`
-                    while (values[0] !== '|') target += values.shift();
+                    while (list[0] !== '|') target += list.shift();
 
 
                     // 将过滤器解析成二维数组
-                    // group => [['filter1'], [['filter2', 'argv1', 'argv2']]
-                    values.filter(v => {
+                    list.filter(v => {
                         return v !== v3split;
                     }).forEach(value => {
                         if (value === '|') {
@@ -162,7 +124,6 @@ const nativeRule = {
 
 
                     // 将过滤器管道化
-                    // code => `$imports.filter2($imports.filter1(value[key]),argv1,argv2)`
                     group.reduce((accumulator, filter) => {
                         const name = filter.shift();
                         filter.unshift(accumulator);
@@ -173,9 +134,12 @@ const nativeRule = {
                 } else if (options.imports[key]) {
 
                     // ... v3 compat ...
-                    // helperName value
                     upgrade('filterName value', 'value | filterName');
-                    code = `${key}(${values.join(',')})`;
+
+                    group = split(esTokens);
+                    group.shift();
+
+                    code = `${key}(${group.join(',')})`;
                     output = 'raw';
 
                 } else {
@@ -214,20 +178,21 @@ const split = (esTokens) => {
         const esToken = esTokens[current];
         const esTokenType = esToken.type;
 
-        if (esTokenType === `whitespace` || esTokenType === `comment`) {
-            current++;
-            continue;
-        } else if (lastToken.type === 'punctuator' && lastToken.value !== ']' || esTokenType === 'punctuator') {
-            group[group.length - 1].push(esToken);
-        } else {
-            group.push([esToken]);
+        if (esTokenType !== `whitespace` && esTokenType !== `comment`) {
+
+            if (lastToken.type === `punctuator` && lastToken.value !== `]` || esTokenType === `punctuator`) {
+                group[group.length - 1].push(esToken);
+            } else {
+                group.push([esToken]);
+            }
+
+            lastToken = esToken;
         }
 
-        lastToken = esToken;
         current++;
     }
 
-    return group.map(g => g.map(g => g.value).join(''));
+    return group.map(g => g.map(g => g.value).join(``));
 };
 
 
