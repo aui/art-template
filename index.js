8c3c21fe2478daedf72349e8824e65e59d52b47e
糖饼 <sugarpie.tang@gmail.com>
2017-04-15 16:48:57 +0800

    main

---
+++ index.js
@@ -0,0 +1,17 @@
+var template = require('./lib/template-node.js');
+var templatePath = require.resolve('./lib/template-node.js');
+var imports = 'var template=require(' + JSON.stringify(templatePath) + ')';
+
+var extension = function (module, flnm) {
+    var filename = flnm || module.filename;
+    var options = JSON.stringify({
+        filename: filename
+    });
+
+    module._compile(imports + '\n' + 'module.exports = template.compile(' + options + ');', filename);
+};
+
+template.extension = extension;
+require.extensions['.art'] = extension;
+
+module.exports = template;
