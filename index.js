b9d13c225b6bafa624cd439fd653e4a35f8e570e
糖饼 <sugarpie.tang@gmail.com>
2017-04-27 21:44:41 +0800

    fix #393

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var template = require('./lib/template-node');
 var extension = require('./lib/extension');
-var precompile = require('./lib/precompile');
 
 template.extension = extension;
-template.precompile = precompile;
 require.extensions[template.defaults.extname] = extension;
 
 module.exports = template;
